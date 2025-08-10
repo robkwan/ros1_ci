ef751d18047a4b2276a811cf043a888d0bcbb99f
Robert Kwan <robert.kwan@gmail.com>
2025-08-10 09:21:52 +0000

    New change to test.

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ set -x
 #Launch tortoisebot gazebo
 echo "$(date +'[%Y-%m-%d %T]') Starting tortoisebot gazebo..."
 roslaunch tortoisebot_gazebo tortoisebot_playground.launch &
-BG_PID=$!
+#BG_PID=$!
 
 # Wait for services
 echo "[$(date '+%Y-%m-%d %T')] Waiting for services to initialize..."
@@ -26,27 +26,27 @@ until rostopic list | grep -q '/gazebo'; do sleep 1; done
 #Launch waypoint action server
 echo "$(date +'[%Y-%m-%d %T]') Starting waypoint action server..."
 rosrun tortoisebot_waypoints tortoisebot_action_server.py &
-AS_PID=$!
+#AS_PID=$!
 
 until rostopic list | grep -q '/tortoisebot_as'; do sleep 1; done
 
 #Launch waypoint action server tests
-echo "$(date +'[%Y-%m-%d %T]') Starting waypoint action server..."
+echo "$(date +'[%Y-%m-%d %T]') Starting waypoint action test..."
 set +e  # Don't exit immediately on test failures; we want to capture code
 rostest tortoisebot_waypoints waypoints_test.test --reuse-master 
-TEST_RESULT=$?
+#TEST_RESULT=$?
 set -e
 
-echo " * RESULT: $( [ $TEST_RESULT -eq 0 ] && echo SUCCESS || echo FAILURE )"
+#echo " * RESULT: $( [ $TEST_RESULT -eq 0 ] && echo SUCCESS || echo FAILURE )"
 
 # Shutdown background processes
-for pid in ${AS_PID:-} ${BG_PID:-}; do
-    if [ -n "$pid" ]; then
-        echo "Stopping PID $pid"
-        kill "$pid" 2>/dev/null || true
-        wait "$pid" 2>/dev/null || true
-    fi
-done
+#for pid in ${AS_PID:-} ${BG_PID:-}; do
+#    if [ -n "$pid" ]; then
+#        echo "Stopping PID $pid"
+#        kill "$pid" 2>/dev/null || true
+#        wait "$pid" 2>/dev/null || true
+#    fi
+#done
 
 # --- Exit with the real test result ---
-exit $TEST_RESULT
+#exit $TEST_RESULT
