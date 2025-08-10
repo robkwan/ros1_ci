770b5f741062f34f757422ff5637fc63b72a1f85
Robert Kwan <robert.kwan@gmail.com>
2025-08-10 09:24:51 +0000

    New change to test 2.

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ set -x
 
 #Launch tortoisebot gazebo
 echo "$(date +'[%Y-%m-%d %T]') Starting tortoisebot gazebo..."
-roslaunch tortoisebot_gazebo tortoisebot_playground.launch &
+roslaunch tortoisebot_gazebo tortoisebot_playground.launch 
 #BG_PID=$!
 
 # Wait for services
@@ -25,7 +25,7 @@ until rostopic list | grep -q '/gazebo'; do sleep 1; done
 
 #Launch waypoint action server
 echo "$(date +'[%Y-%m-%d %T]') Starting waypoint action server..."
-rosrun tortoisebot_waypoints tortoisebot_action_server.py &
+rosrun tortoisebot_waypoints tortoisebot_action_server.py 
 #AS_PID=$!
 
 until rostopic list | grep -q '/tortoisebot_as'; do sleep 1; done
