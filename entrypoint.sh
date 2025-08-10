cd1d2b434097c4fccaafd593a5aec7d76b3ea67e
Robert Kwan <robert.kwan@gmail.com>
2025-08-10 07:15:22 +0000

    Modified to allow jenkins test end correctly again.

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ until rostopic list | grep -q '/tortoisebot_as'; do sleep 1; done
 
 #Launch waypoint action server tests
 echo "$(date +'[%Y-%m-%d %T]') Starting waypoint action server..."
-rostest tortoisebot_waypoints waypoints_test.test --reuse-master rostest tortoisebot_waypoints waypoints_test.test --reuse-master
+rostest tortoisebot_waypoints waypoints_test.test --reuse-master 
 TEST_RESULT=$?
 
 # Clean up background processes
